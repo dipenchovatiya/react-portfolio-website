4f2e0de02cf7ca98af50125e364a0bff8e9f4089
Dipen Chovatiya <dipenchovatiya07@gmail.com>
2022-09-17 19:50:59 +0530

    added entire project

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import CV from '../../assets/cv.pdf'
 
-function CTA() {
+const CTA = () => {
   return (
     <div className='cta'>
-        <a href={CV} download className='btn'>Download Resume</a>
+        <a href={CV} download className='btn'>Download CV</a>
         <a href="#contact" className='btn btn-primary'>Let's Talk</a>
     </div>
   )
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import {BsLinkedin} from 'react-icons/bs'
-import {FaGithubSquare} from 'react-icons/fa'
-import {FiInstagram} from 'react-icons/fi'
-function HeaderSocials() {
+import {FaGithub} from 'react-icons/fa'
+import {FiDribbble} from 'react-icons/fi'
+
+const HeaderSocials = () => {
   return (
-    <div className='header_socials'>
-        <a href="https://linkedin.com/dipenchovatiya" target="_blank"><BsLinkedin/></a>
-        <a href="https://github.com/dipenchovatiya" target="_blank"><FaGithubSquare/></a>
-        <a href="https://instagram.com/dipenchovatiya" target="_blank"><FiInstagram/></a>
+    <div className='header__socials'>
+        <a href="https://linkedin.com" target="_blank"><BsLinkedin/></a>
+        <a href="https://github.com" target="_blank"><FaGithub/></a>
+        <a href="https://dribbble.com" target="_blank"><FiDribbble/></a>
     </div>
   )
 }
