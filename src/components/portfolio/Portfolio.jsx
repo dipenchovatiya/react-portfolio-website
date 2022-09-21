82168bafe7e06fdb7fc698a8e034546ec1f6fe36
Dipen Chovatiya <dipenchovatiya07@gmail.com>
2022-09-21 17:23:26 +0530

    updated portfolio section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,56 +1,30 @@
 import React from 'react'
 import './portfolio.css'
-import IMG1 from '../../assets/portfolio1.jpg'
+import IMG1 from '../../assets/portfolio1.svg'
 import IMG2 from '../../assets/portfolio2.jpg'
-import IMG3 from '../../assets/portfolio3.jpg'
-import IMG4 from '../../assets/portfolio4.jpg'
-import IMG5 from '../../assets/portfolio5.png'
-import IMG6 from '../../assets/portfolio6.jpg'
-
-// DO NOT USE THE IMAGES IN PRODUCTION
+import IMG3 from '../../assets/portfolio3.png'
 
 const data = [
   {
     id: 1,
     image: IMG1,
-    title: 'Crypto Currency Dashboard & Financial Visualization',
-    github: 'https://github.com',
-    demo: 'https://dribbble.com/shots/16673715-Crypto-currency-dashboards-and-financial-data-visualization'
+    title: 'Data Analysis on population of US counties using Choropleth map',
+    github: 'https://github.com/dipenchovatiya/Python-Reports/tree/master/Choropleth%20of%20US',
+    demo: 'https://github.com/dipenchovatiya/Python-Reports/blob/master/Choropleth%20of%20US/Choropleth.ipynb'
   },
   {
     id: 2,
     image: IMG2,
-    title: 'Charts templates & infographics in Figma',
-    github: 'https://github.com',
-    demo: 'https://dribbble.com/shots/16580766-Orion-UI-kit-Charts-templates-infographics-in-Figma'
+    title: 'Training a Neural Net for Image Classification',
+    github: 'https://github.com/dipenchovatiya/Python-Reports/tree/master/NN_Image_Classification',
+    demo: 'https://github.com/dipenchovatiya/Python-Reports/blob/master/NN_Image_Classification/NN_Image_Classification.ipynb'
   },
   {
     id: 3,
     image: IMG3,
-    title: 'Figma dashboard UI kit for data design web apps',
-    github: 'https://github.com',
-    demo: 'https://dribbble.com/shots/17290917-Eclipse-Figma-dashboard-UI-kit-for-data-design-web-apps'
-  },
-  {
-    id: 4,
-    image: IMG4,
-    title: 'Maintaining tasks and tracking progress',
-    github: 'https://github.com',
-    demo: 'https://dribbble.com/shots/16955822-Maintaining-tasks-and-tracking-progress'
-  },
-  {
-    id: 5,
-    image: IMG5,
-    title: 'Charts templates & infographics in Figma',
-    github: 'https://github.com',
-    demo: 'https://dribbble.com/shots/16541289-Orion-UI-kit-Charts-templates-infographics-in-Figma'
-  },
-  {
-    id: 6,
-    image: IMG6,
-    title: 'Charts templates & infographics in Figma',
-    github: 'https://github.com',
-    demo: 'https://dribbble.com/shots/15887665-Orion-UI-kit-Charts-templates-infographics-in-Figma'
+    title: 'Designed and developed Garment Store Website to attract more customers',
+    github: 'https://github.com/dipenchovatiya/krishcreationgarment.github.io',
+    demo: 'https://krishcreationgarment.github.io/'
   }
 ]
 
