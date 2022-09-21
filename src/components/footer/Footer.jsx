a7db597eabea6dd7eb5a1044945b511301dfeebf
Dipen Chovatiya <dipenchovatiya07@gmail.com>
2022-09-21 19:07:09 +0530

    Updated Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,15 +7,13 @@ import {IoLogoTwitter} from 'react-icons/io'
 const Footer = () => {
   return (
     <footer>
-      <a href="#" className='footer__logo'>EGATOR</a>
+      <a href="#" className='footer__logo'>DIPEN</a>
 
       <ul className='permalinks'>
         <li><a href="#">Home</a></li>
         <li><a href="#about">About</a></li>
-        <li><a href="#experience">Expeirience</a></li>
-        <li><a href="#services">Services</a></li>
+        <li><a href="#experience">Experience</a></li>
         <li><a href="#portfolio">Portfolio</a></li>
-        <li><a href="#testimonials">Testimonials</a></li>
         <li><a href="#contact">Contact</a></li>
       </ul>
 
@@ -26,7 +24,7 @@ const Footer = () => {
       </div>
 
       <div className="footer__copyright">
-        <small>&copy; EGATOR Tutorials. All rights reserved.</small>
+        <small> Chardip Tech &copy; 2022. All rights reserved.</small>
       </div>
     </footer>
   )
